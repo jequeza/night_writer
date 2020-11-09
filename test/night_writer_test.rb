fd8b405ba7efa3ef47f8ea8e004655de2bcb1a7d
Jesus Quezada-Guillen <maj3st1@gmail.com>
2020-11-09 15:34:02 -0600

    Add test for display_message WIP.

--- test/night_writer_test.rb
+++ test/night_writer_test.rb
@@ -6,7 +6,18 @@ class NightWriterTest < Minitest::Test
   def test_it_exists_and_has_attributes
     nite_writer = NightWriter.new
     assert_instance_of NightWriter, nite_writer
-    assert_equal ARGV[0], night_writer.file_in
-    assert_equal ARGV[1], night_writer.file_out
+    assert_equal ARGV[0], nite_writer.file_in
+    assert_equal ARGV[1], nite_writer.file_out
   end
+
+  def test_it_can_display_message
+    nite_writer = NightWriter.new
+    file_out = "output.txt"
+    @incoming_text = "hello"
+    expected_msg = "Created #{@file_out} containing #{@incoming_text.length} characters."
+
+    assert_equal expected_msg, nite_writer.display_message
+  end
+
+
 end
